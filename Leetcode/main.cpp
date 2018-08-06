6ecc868fe1a13e3184f3b19768ccea865228410a
fangxi <fangxi@fangxidemacbook-pro.local>
2018-08-06 12:50:58 +0800

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by fangxi on 2018/8/6.
+//  Copyright © 2018年 fangxi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
